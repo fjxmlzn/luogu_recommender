a18d058b2dadc847ea07b4908d8568416105aa92
lzn <linzinan1995@126.com>
2017-07-26 14:59:33 +0800

    add code

---
+++ config.py
@@ -0,0 +1,20 @@
+# database
+HOST = "localhost"
+USER = "root"
+PASSWORD = "root"
+DB = "luogu"
+PORT = 3306
+CHARSET = "utf8"
+
+# system parameters
+REC_MATRIX_FILE = "rec_matrix.pkl"
+
+# train parameters
+TRAIN_WEIGHT = [0, 0.8, 0.4, 0.2, 0.1]
+
+# test parameters
+DECAY_RATE = 0.8
+
+# validate parameters
+HISTORY_LENGTH = 3
+
